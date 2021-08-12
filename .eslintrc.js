4e7f467bd3f77cd6d75634bdd741b0df8c040023
Andrew Huynh <andrew.huynh17@gmail.com>
2021-08-12 19:07:01 -0400

    package update

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "airbnb",
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
