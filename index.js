f18c40b3c29e78964d81f4ac1098cc518257443d
Andrew Huynh <andrew.huynh17@gmail.com>
2021-08-21 19:54:41 -0400

    added auth headers  to socket

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
+const jwt = require("jsonwebtoken");
+const User = require("./model/user");
 const app = require("./app");
 const httpServer = require("http").Server(app);
 const options = {
   cors: { origin: true },
 };
 const io = require("socket.io")(httpServer, options);
-app.set("socketio", io);
 const getKebab = require("./socket/handleGetKebab"); //routes
+app.set("socketio", io);
+
 // for https makes sure you get your ssl certs
-
-//already connected to mongodb in express app
-
+io.use(async (socket, next) => {
+  let token = socket.handshake.auth.token;
+  if (token) {
+    token = token.split(" ");
+    if (token.length > 0 && token[0].includes("Bearer")) {
+      const jwtVerify = jwt.verify(token[1], process.env.JWT_SECRET);
+      socket.token = jwtVerify;
+      socket.user = await User.findOne({ _id: jwtVerify.id });
+    }
+  }
+  next();
+});
 const onConnection = (socket) => {
   //function for on connection instead of writing it inline
   console.log("Connected:", socket.id);
-  // console.log(socket.request.user);
-
+  // console.log(socket.request);
   socket.on("disconnect", () => {
     //listening when disconnect
     console.log("ID:", socket.id, "disconnected");
     socket.removeAllListeners();
   });
 
-  getKebab(io, socket); // routes
+  if (socket.user) {
+    getKebab(io, socket);
+  } // routes}
 };
 io.on("connect", onConnection); // on connection do these tasks
 
